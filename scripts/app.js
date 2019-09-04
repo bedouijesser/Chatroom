b5cd31757a8efe0ad282bf9d23173056a166a3f9
Hidora <bedouijesser@gmail.com>
2019-09-04 14:07:10 +0100

    improvments?

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,13 @@ const msgField = document.querySelector('#msg');
 const chNameField = document.querySelector('#user-name')
 const topicBtns = document.querySelectorAll('body > div > div.chat-rooms.mb-3.text-center > button');
 
+
+
+
+
+
+
+// add a new user to the database
 const logNewUser =() =>{
     const newUser = {
         id: "Anonym",
@@ -11,7 +18,20 @@ const logNewUser =() =>{
     users.add(newUser);
 }
 
+// delete user on exit ..? (still testing)
+window.onbeforeunload = function (e) {
+    var message = "Are you fuckin sure?";
+    localStorage.add("key","this cond worked");
 
+    var e = e || window.event;
+    // For IE and Firefox
+    if (e) {
+      e.returnValue = message;
+    }
+  
+    // For Safari
+    return message;
+  };
 
 
 // event listener for the chatroom buttons
